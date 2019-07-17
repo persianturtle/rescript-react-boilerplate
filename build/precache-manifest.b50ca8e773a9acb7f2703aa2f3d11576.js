b1980589af2e7f7fb9691402b65721154001ba98
Raphael Rafatpanah <persianturtle@gmail.com>
2019-07-16 20:53:15 -0400

    Build

--- build/precache-manifest.b50ca8e773a9acb7f2703aa2f3d11576.js
+++ build/precache-manifest.b50ca8e773a9acb7f2703aa2f3d11576.js
@@ -1,9 +1,9 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ff0753686834f9ae4365a2ae7b156201",
+    "revision": "84d0c746dc497c2bf4d01c936be25a13",
     "url": "index.html"
   },
   {
-    "url": "main.cb7fd5053ac25382610f.js"
+    "url": "main.8694bd61153c8083a1ee.js"
   }
 ]);
--- build/sw.js
+++ build/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.381e2946882b9682221ed5b4cf0dfd90.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.b50ca8e773a9acb7f2703aa2f3d11576.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 workbox.routing.registerNavigationRoute("/index.html");
