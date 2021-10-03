6781b0f25db8a42ca76c2e624e4dc988607304ff
Alvaro <astaboada7@gmail.com>
2021-10-03 20:38:15 +0200

    fixed webpack error

--- build/precache-manifest.9d92c417be162d3ecd38aa9bbb2df120.js
+++
@@ -1,9 +0,0 @@
-self.__precacheManifest = (self.__precacheManifest || []).concat([
-  {
-    "revision": "b69015147cd03315b8419ce29659ca4d",
-    "url": "index.html"
-  },
-  {
-    "url": "main.dfd37307a0b9c476bfe7.js"
-  }
-]);
