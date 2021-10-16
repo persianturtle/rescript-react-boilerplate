c06a921d164e5a19116e0d2612bfafb646c211c1
Alvaro <astaboada7@gmail.com>
2021-10-16 19:03:06 +0200

    switch to esbuild, much faster

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ rm -rf dist
 mkdir dist
 cp src/index.html dist
 npx rescript
-npx esbuild lib/es6/src/Index.bs.js --bundle --minify --outfile=dist/app.js
+npx esbuild lib/es6/src/Index.bs.js --bundle --loader:.svg=file --minify --outfile=dist/app.js
--- scripts/start.js
+++ scripts/start.js
@@ -1,15 +1,15 @@
 const process = require("child_process");
 const chokidar = require("chokidar");
 
-//build();
-//serve();
+build();
+serve();
 
 chokidar.watch("src/**/*", { ignoreInitial: true }).on("all", build);
 
 function build() {
-  process.spawn("sh", ["scripts/build.sh"], { stdio: "inherit" });
+  process.spawn("sh", ["scripts/build.sh"], { stdio: "inherit", shell: true });
 }
 
 function serve() {
-  process.spawn("npx", ["http-server", "-c-1", "-p", '0', "dist"], { stdio: "inherit" });
+  process.spawn("npx", ["http-server", "-c-1", "--proxy", "http://localhost:8081?", "-p", "8081", "dist"], { stdio: "inherit", shell: true });
 }
