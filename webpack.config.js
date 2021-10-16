0282ebea292e4ddc8c83915c57cf28530f47b5f3
Raphael Rafatpanah <persianturtle@gmail.com>
2021-10-16 11:56:46 -0400

    Remove PWA features temporarily

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,23 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const WorkboxPlugin = require('workbox-webpack-plugin')
-const WebpackPwaManifest = require('webpack-pwa-manifest')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './lib/es6/src/Index.bs.js',
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: "./lib/es6/src/Index.bs.js",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   output: {
-    path: path.join(__dirname, 'build/'),
-    filename: '[name].[chunkhash].js'
+    path: path.join(__dirname, "build/"),
+    filename: "[name].[chunkhash].js",
   },
   module: {
-    rules: [{ test: /\.(png|jpg|svg)$/, use: 'url-loader' }]
+    rules: [{ test: /\.(png|jpg|svg)$/, use: "url-loader" }],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    //new WorkboxPlugin.InjectManifest({
-     // swSrc: './src/sw.js'
-    //}),
-    new WebpackPwaManifest({
-      name: 'App Shell Boilerplate',
-      short_name: 'AppShell',
-      description: 'This is a description!',
-      background_color: '#ffffff',
-      icons: [
-        {
-          src: path.resolve('src/img/icon/icon.png'),
-          size: '400x400'
-        }
-      ]
+      template: "./src/index.html",
     }),
     new CleanWebpackPlugin({
-      watch: true
-    })
-  ]
-}
+      watch: true,
+    }),
+  ],
+};
