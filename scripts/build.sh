9fef871e02607a039eb950a577478a654b84c0e5
Raphael Rafatpanah <persianturtle@gmail.com>
2021-10-17 11:22:46 -0400

    Copy 404.html file to dist

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 rm -rf dist
 mkdir dist
-cp -r src/index.html img dist
+cp -r src/index.html img 404.html dist
 npx rescript
 npx esbuild lib/es6/src/Index.bs.js --bundle --loader:.svg=file --minify --outfile=dist/app.js
