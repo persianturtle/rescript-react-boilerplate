1acdf2672da322ab7d9ddd7bf803fe75deb331e1
Raphael Rafatpanah <persianturtle@gmail.com>
2021-10-16 21:59:14 -0400

    Minify build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ rm -rf dist
 mkdir dist
 cp src/index.html dist
 npx rescript
-npx esbuild lib/es6/src/Index.bs.js --bundle --loader:.svg=file --outfile=dist/app.js
+npx esbuild lib/es6/src/Index.bs.js --bundle --loader:.svg=file --minify --outfile=dist/app.js
 
