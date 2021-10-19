eaae139972e31997ac9f1e421d3445d14cfc31f4
Raphael Rafatpanah <persianturtle@gmail.com>
2021-10-18 23:35:09 -0400

    Build

--- dist/sw.js
+++ dist/sw.js
@@ -1,52 +1,61 @@
 
-// Force the service worker to change if index.html changes
-const indexHash = "05bfac3eae71c56833c5295a6c36a6b9e62b0b211806fdd0d85cb0ebbda679e9";
-const filesToCache = ["/404.html","/app-AHOLRMYQ.js","/img/demo.gif","/img/start.png","/"];
-const currentCacheName = "rescript-react-boilerplate-4d2c2e41d4097eb894662386b60839302685e34d6ad6127d3a556f6a3ead94cc";
+const filesToCache = ["/404.html","/app-AHOLRMYQ.js","/favicon.ico","/img/demo.gif","/img/start.png","/"];
+const currentCacheName = "rescript-react-boilerplate-0427f7da01d9be674e4b7e8cf139be80a40c340d91c45395b2da73e2c0b349a4";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(currentCacheName)
-      .then((cache) =>
-        cache.addAll(["/404.html","/app-AHOLRMYQ.js","/img/demo.gif","/img/start.png","/"])
-      )
+    caches.open(currentCacheName).then((cache) => cache.addAll(filesToCache))
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  if (
-    filesToCache
-      .map((file) => self.location.protocol + "//" + self.location.host + file)
-      .includes(event.request.url)
-  ) {
-    return event.respondWith(caches.match(event.request));
-  }
+  event.respondWith(
+    (async () => {
+      const cachedResponse = await caches.match(event.request);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
 
-  return fetch(event.request)
-});
+      try {
+        return (await event.preloadResponse) ?? (await fetch(event.request));
+      } catch (error) {
+        if (event.request.mode === "navigate") {
+          return caches.match("/");
+        }
 
-self.addEventListener("activate", async (event) => {
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) =>
-        Promise.all(
-          cacheNames
-            .filter(
-              (cacheName) =>
-                cacheName.startsWith("rescript-react-boilerplate") &&
-                cacheName !== currentCacheName
-            )
-            .map(cacheName => caches.delete(cacheName))
-        )
-      )
+        throw error;
+      }
+    })()
   );
 });
 
-self.addEventListener("message", event => {
+addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      if (self.registration.navigationPreload) {
+        await self.registration.navigationPreload.enable();
+      }
+
+      return caches
+        .keys()
+        .then((cacheNames) =>
+          Promise.all(
+            cacheNames
+              .filter(
+                (cacheName) =>
+                  cacheName.startsWith("rescript-react-boilerplate") &&
+                  cacheName !== currentCacheName
+              )
+              .map((cacheName) => caches.delete(cacheName))
+          )
+        );
+    })()
+  );
+});
+
+self.addEventListener("message", (event) => {
   if (event.data.action === "skipWaiting") {
-    self.skipWaiting()
+    self.skipWaiting();
   }
-})
+});
     
