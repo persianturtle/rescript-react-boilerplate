93cb8afb79280dd7ee27b6b6275db636ece13d5d
Raphael Rafatpanah <persianturtle@gmail.com>
2021-10-18 20:30:16 -0400

    Add ServiceWorker

---
+++ scripts/injectScript.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+function injectScript() {
+  const fileName = fs
+    .readdirSync("dist")
+    .find((file) => file.startsWith("app") && file.endsWith(".js"));
+
+  const file = fs
+    .readFileSync("dist/index.html", "utf-8")
+    .replace("</body>", `<script src="${fileName}"></script></body>`);
+
+  fs.writeFileSync("dist/index.html", file, "utf-8");
+}
+
+module.exports = injectScript;
