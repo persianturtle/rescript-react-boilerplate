e3434200abf5998cc48407453a031bf660a7db8c
Raphael Rafatpanah <persianturtle@gmail.com>
2021-10-18 23:44:16 -0400

    Updates

---
+++ sw.js
@@ -0,0 +1,61 @@
+
+const filesToCache = ["/app-AHOLRMYQ.js","/favicon.ico","/img/demo.gif","/img/start.png","/"];
+const currentCacheName = "rescript-react-boilerplate-f58678b4d2f71556adee75a16d8c3302dfa5f80a1bcff8384173db7a178e501a";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(currentCacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const cachedResponse = await caches.match(event.request);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      try {
+        return (await event.preloadResponse) ?? (await fetch(event.request));
+      } catch (error) {
+        if (event.request.mode === "navigate") {
+          return caches.match("/");
+        }
+
+        throw error;
+      }
+    })()
+  );
+});
+
+addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      if (self.registration.navigationPreload) {
+        await self.registration.navigationPreload.enable();
+      }
+
+      return caches
+        .keys()
+        .then((cacheNames) =>
+          Promise.all(
+            cacheNames
+              .filter(
+                (cacheName) =>
+                  cacheName.startsWith("rescript-react-boilerplate") &&
+                  cacheName !== currentCacheName
+              )
+              .map((cacheName) => caches.delete(cacheName))
+          )
+        );
+    })()
+  );
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data.action === "skipWaiting") {
+    self.skipWaiting();
+  }
+});
+    
