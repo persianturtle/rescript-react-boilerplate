0bd1474d96d37809227abf1706270450025ba64a
Raphael Rafatpanah <persianturtle@gmail.com>
2021-10-20 23:08:29 -0400

    Build

--- dist/sw.js
+++ dist/sw.js
@@ -1,6 +1,6 @@
 
-const filesToCache = ["/app-67LGDKSS.js","/favicon.ico","/img/demo.gif","/img/icon/arrow.svg","/img/icon/chevron.svg","/img/icon/hamburger.svg","/img/icon/rescript_256x256.png","/img/icon/rescript_512x512.png","/img/rescript.svg","/img/start.png","/","/manifest.json"];
-const currentCacheName = "rescript-react-boilerplate-2dd045988f02893a841b25e04b5134d1e443103e6f458005497085b5445c14ae";
+const filesToCache = ["/app-XVVDLNP4.js","/favicon.ico","/img/demo.gif","/img/icon/arrow.svg","/img/icon/chevron.svg","/img/icon/hamburger.svg","/img/icon/rescript_256x256.png","/img/icon/rescript_512x512.png","/img/rescript.svg","/img/start.png","/","/manifest.json"];
+const currentCacheName = "rescript-react-boilerplate-01caa510b724cb029062e0fb866949ffb04a0ac056e5f907f59fef4d70612bfa";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
